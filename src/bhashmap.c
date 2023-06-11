57cfbeb229b1527b290d0e51ad87e9ab8d79828c
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2023-06-11 12:10:30 +0200

    Initial skeleton implementation.

--- src/bhashmap.c
+++ src/bhashmap.c
@@ -3,7 +3,8 @@ BHashMap is a generic Hash Table implementation written in C11. It was initially
 of the hash table data structure, however it has since evolved to be a very solid, usable implementation.
 
 The goal is to provide performant, general hash table implementation with a pleasant-to-use API that allows for keys and values of arbitrary types
-and sizes. The goal is NOT to provide a specific hyper-optimized implementation for any one specific use case.
+and sizes. The goal is NOT to provide a specific hyper-optimized implementation for any one specific use case. For example, the API allows
+for multi-gigabyte memory regions to be used as keys, without any restrictions. While this is of course not ideal, it's possible.
 
 If the macro BHM_DEBUG is defined, the functions throughout the library print various
 debug messages and info to stderr.
@@ -17,6 +18,9 @@ debug messages and info to stderr.
 #include <sys/types.h>
 
 #include "bhashmap.h"
+
+#define BHM_LOAD_FACTOR_LIMIT 0.7
+#define BHM_RESIZE_FACTOR 2
 
 /*
 The DEBUG_PRINT macro only expands if BHM_DEBUG is defined. Otherwise, it expands to nothing
@@ -32,7 +36,9 @@ and as such no print is performed.
 #define HASH(dataptr, len) murmur3_32(dataptr, len, 1u)
 
 typedef struct HashPair {
-    void *key, *value;
+    void *key;
+    size_t keylen;
+    const void *value;
     struct HashPair *next;
 } HashPair;
 
@@ -40,7 +46,7 @@ struct BHashMap {
     size_t capacity,
            pair_count;
 
-    HashPair *table;
+    HashPair *buckets;
 };
 
 // Murmurhash3 implementation
@@ -95,6 +101,11 @@ murmur3_32(const char *key, uint32_t len, uint32_t seed) {
     return hash;
 }
 
+static inline double
+get_load_factor(BHashMap *map) {
+    return (double) map->pair_count / (double) map->capacity;
+}
+
 /*
 Create a new BHashMap with a given capacity.
 RETURN VALUE:
@@ -112,10 +123,10 @@ bhm_create(const size_t capacity) {
     *new_map = (BHashMap) {
         .capacity = capacity,
         .pair_count = 0,
-        .table = calloc(capacity, sizeof(HashPair))
+        .buckets = calloc(capacity, sizeof(HashPair))
     };
 
-    if (!new_map->table) {
+    if (!new_map->buckets) {
         free(new_map);
         return NULL;
     }
@@ -123,4 +134,148 @@ bhm_create(const size_t capacity) {
     DEBUG_PRINT("Created hash map with capacity %lu.\n", capacity);
 
     return new_map;
+}
+
+/*
+Find and return the appropriate bucket for a given key,
+based on its hash and the capacity of the hashmap.
+*/
+static HashPair *
+find_bucket(BHashMap *map, const void *key, const size_t keylen) {
+    const uint32_t hash       = HASH(key, keylen),
+                   bucket_idx = hash % map->capacity;
+
+    DEBUG_PRINT("KEY: '%s', BUCKET IDX: %u\n", (char * key), bucket_idx);
+
+    return &map->buckets[bucket_idx];
+}
+
+/*
+Insert a key-value pair into a HashPair structure.
+*/
+static inline bool
+insert_pair(HashPair *pair, const void *key, const size_t keylen, const void *data) {
+    pair->key = malloc(keylen);
+    pair->keylen = keylen;
+
+    if (!pair->key) {
+        return false;
+    }
+
+    memcpy(pair->key, key, keylen);
+    pair->value = data;
+
+    return true;
+}
+
+/*
+Insert a new key-value pair into the hashmap, or update the associated value if the key already
+exists in the hashmap.
+
+"keylen" is the length of the key in bytes.
+
+RETURN VALUE:
+    On success, true is returned.
+    On failure, false is returned.
+*/
+bool
+bhm_set(BHashMap *map, const void *key, const size_t keylen, const void *data) {
+    HashPair *bucket = find_bucket(map, key, keylen);
+    
+    /* best case - given bucket completely empty - insert key-value pari*/
+    if (bucket->key == NULL) {
+        if (!insert_pair(bucket, key, keylen, data)) {
+            return false;
+        }
+
+        map->pair_count += 1;
+
+        return true;
+    }
+
+    HashPair *head = bucket;
+
+    while (head) {
+        if (head->keylen == keylen && memcmp(key, head->key, keylen) == 0) {
+            /* found the key already in the map - update its value */
+            head->value = data;
+            return true;
+        }
+
+        if (head->next == NULL) {
+            /* at end of linked list - allocate space for new pair and copy data over */
+            HashPair *new_pair = malloc(sizeof(HashPair));
+            if (!new_pair) {
+                return false;
+            }
+
+            if (!insert_pair(new_pair, key, keylen, data)) {
+                free(new_pair);
+                return false;
+            }
+
+            new_pair->next = NULL;
+            head->next = new_pair;
+
+            map->pair_count += 1;
+
+            return true;
+        }
+
+        head = head->next;
+    }
+
+    return false; // ERROR ?
+}
+
+/*
+Get the value of a key from the map.
+RETURN VALUE:
+    NULL     - key not found / error
+    NON-NULL - appropriate data
+*/
+void *
+bhm_get(BHashMap *map, const void *key, const size_t keylen) {
+    HashPair *bucket = find_bucket(map, key, keylen);
+
+    /* empty bucket, key definitely not in map */
+    if (!bucket->key) {
+        return NULL;
+    }
+
+    while (bucket) {
+        if (bucket->keylen == keylen && memcmp(key, bucket->key, keylen) == 0) {
+            return (void *) bucket->value;
+        }
+
+        bucket = bucket->next;
+    }
+
+    return NULL;
+}
+
+/*
+Free all resources occupied by the hash map. This includes the memory of the main BHashMap
+structure, the memory for all the hash pairs in the structure (those at the 'root' as well as 
+those in the linked lists), as well as memory for all the copied keys.
+*/
+void
+bhm_destroy(BHashMap *map) {
+    for (size_t i = 0; i < map->capacity; i++) {
+        free(map->buckets[i].key);
+
+        HashPair *head = map->buckets[i].next;
+
+        while (head) {
+            HashPair *n = head->next;
+
+            free(head->key);
+            free(head);
+
+            head = n;
+        }
+    }
+
+    free(map->buckets);
+    free(map);
 }
--- src/include/bhashmap.h
+++ src/include/bhashmap.h
@@ -1,6 +1,16 @@
 #include <sys/types.h>
+#include <stdbool.h>
 
 typedef struct BHashMap BHashMap;
 
 BHashMap *
-bhm_create(const size_t capacity); 
+bhm_create(const size_t capacity);
+
+bool
+bhm_set(BHashMap *map, const void *key, const size_t keylen, const void *data); 
+
+void *
+bhm_get(BHashMap *map, const void *key, const size_t keylen); 
+
+void
+bhm_destroy(BHashMap *map); 
