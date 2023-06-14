a6cb1179930eea06ad9df373081fa84b10132238
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2023-06-14 09:33:33 +0000

    Benchmarks: use average time of iterations

--- src/benchmarks/benchmark_ints.c
+++ src/benchmarks/benchmark_ints.c
@@ -7,13 +7,15 @@
 
 #include "bhashmap.h"
 
-#define ITERATIONS 16
+#define ITERATIONS 32
 
 int main(void) {
     struct timespec time_start, time_end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);
+    size_t total_time_ms = 0;
 
     for (size_t j = 0; j < ITERATIONS; j++) {
+        clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);
+
         BHashMap *map = bhm_create(32);
         if (!map) {
             fprintf(stderr, "error\n");
@@ -31,12 +33,13 @@ int main(void) {
         }
 
         bhm_destroy(map);
+
+        clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);
+        uint64_t ms_elapsed  = ((time_end.tv_sec * 1000000000 + time_end.tv_nsec) - (time_start.tv_sec * 1000000000 + time_start.tv_nsec)) / 1000000;
+        total_time_ms += ms_elapsed;
     }
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);
+    fprintf(stderr, "Avg. time of %d ITERATIONS: %lums\n", ITERATIONS, total_time_ms / ITERATIONS);
 
-    uint64_t nano_elapsed = (time_end.tv_sec * 1000000000 + time_end.tv_nsec) - (time_start.tv_sec * 1000000000 + time_start.tv_nsec);
-
-    printf("%d iterations took: %lums\n", ITERATIONS, nano_elapsed / 1000000);
     return EXIT_SUCCESS;
 }
--- src/benchmarks/benchmark_words400k.c
+++ src/benchmarks/benchmark_words400k.c
@@ -8,7 +8,7 @@
 #include "bhashmap.h"
 
 #define MAXWORDLEN 50
-#define ITERATIONS 1
+#define ITERATIONS 32
 
 int main(void) {
     char (*words)[MAXWORDLEN] = malloc(WORDSTXT_COUNT * MAXWORDLEN);
@@ -22,16 +22,16 @@ int main(void) {
     fclose(f);
 
     struct timespec time_start, time_end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);
+    size_t total_time_ms = 0;
 
     for (size_t j = 0; j < ITERATIONS; j++) {
-        BHashMap *map = bhm_create(32);
+        clock_gettime(CLOCK_MONOTONIC_RAW, &time_start);
 
+        BHashMap *map = bhm_create(32);
         if (!map) {
             fprintf(stderr, "Error creating hash table.\n");
             exit(1);
         }
-
         for (size_t i = 0; i < WORDSTXT_COUNT; i++) {
             if (!bhm_set(map, words[i], strlen(words[i]) + 1, words[i])) {
                 fprintf(stderr, "Error setting key.\n");
@@ -39,15 +39,16 @@ int main(void) {
                 exit(1);
             }
         }
-
         bhm_destroy(map);
+
+        clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);
+        uint64_t ms_elapsed = ((time_end.tv_sec * 1000000000 + time_end.tv_nsec) - (time_start.tv_sec * 1000000000 + time_start.tv_nsec)) / 1000000;
+
+        total_time_ms += ms_elapsed;
     }
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &time_end);
+    fprintf(stderr, "Avg. time of %d ITERATIONS: %lums\n", ITERATIONS, total_time_ms / ITERATIONS);
 
-    uint64_t nano_elapsed = (time_end.tv_sec * 1000000000 + time_end.tv_nsec) - (time_start.tv_sec * 1000000000 + time_start.tv_nsec);
-
-    printf("%d iterations took: %lums\n", ITERATIONS, nano_elapsed / 1000000);
 
     free(words);
     return EXIT_SUCCESS;
