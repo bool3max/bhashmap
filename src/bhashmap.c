eaeb0eaf970fb76d1954cd5093812ff704e66e53
Bogdan Mitrović <bokisa.mitrovic2@gmail.com>
2023-06-15 08:01:40 +0000

    Moved default murmurhash3 impl. to separate header;

--- src/bhashmap.c
+++ src/bhashmap.c
@@ -6,7 +6,10 @@
 #include <sys/types.h>
 
 #include "bhashmap.h"
+#include "murmurhash3.h"
 #include "benchmark.h"
+
+#define HASH(dataptr, datalen) murmur3_32(dataptr, datalen, 1u)
 
 #define BHM_LOAD_FACTOR_LIMIT 0.75
 #define BHM_RESIZE_FACTOR 2
@@ -21,8 +24,6 @@ and as such no print is performed.
 #else
 #define DEBUG_PRINT(fmt, ...)
 #endif
-
-#define HASH(dataptr, datalen) murmur3_32(dataptr, datalen, 1u)
 
 typedef struct HashPair {
     size_t keylen;
@@ -51,58 +52,6 @@ free_buckets(HashPair **buckets, const size_t bucket_count);
 
 static inline HashPair *
 create_pair(const size_t keylen); 
-
-// Murmurhash3 implementation
-uint32_t
-murmur3_32(const char *key, uint32_t len, uint32_t seed) {
-    static const uint32_t c1 = 0xcc9e2d51;
-    static const uint32_t c2 = 0x1b873593;
-    static const uint32_t r1 = 15;
-    static const uint32_t r2 = 13;
-    static const uint32_t m = 5;
-    static const uint32_t n = 0xe6546b64;
-
-    uint32_t hash = seed;
-
-    const int nblocks = len / 4;
-    const uint32_t *blocks = (const uint32_t *) key;
-    int i;
-    for (i = 0; i < nblocks; i++) {
-        uint32_t k = blocks[i];
-        k *= c1;
-        k = (k << r1) | (k >> (32 - r1));
-        k *= c2;
-
-        hash ^= k;
-        hash = ((hash << r2) | (hash >> (32 - r2))) * m + n;
-    }
-
-    const uint8_t *tail = (const uint8_t *) (key + nblocks * 4);
-    uint32_t k1 = 0;
-
-    switch (len & 3) {
-    case 3:
-        k1 ^= tail[2] << 16;
-    case 2:
-        k1 ^= tail[1] << 8;
-    case 1:
-        k1 ^= tail[0];
-
-        k1 *= c1;
-        k1 = (k1 << r1) | (k1 >> (32 - r1));
-        k1 *= c2;
-        hash ^= k1;
-    }
-
-    hash ^= len;
-    hash ^= (hash >> 16);
-    hash *= 0x85ebca6b;
-    hash ^= (hash >> 13);
-    hash *= 0xc2b2ae35;
-    hash ^= (hash >> 16);
-
-    return hash;
-}
 
 static inline double
 get_load_factor(const BHashMap *map) {
